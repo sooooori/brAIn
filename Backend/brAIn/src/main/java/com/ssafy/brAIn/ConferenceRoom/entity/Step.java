1ba9b93029f97e3bad6c29775f59fda12671b3bc
박병준 <h0568@naver.com>
2024-07-22 14:25:29 +0900

    [fix] delete old ConferenceRoom

--- Backend/brAIn/src/main/java/com/ssafy/brAIn/ConferenceRoom/entity/Step.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.brAIn.ConferenceRoom.entity;
-
-public enum Step{
-    STEP_0, STEP_1, STEP_2, STEP_3, STEP_4, STEP_5;
-}
--- Backend/brAIn/src/main/java/com/ssafy/brAIn/ConferenceRoom/repository/ConferenceRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.brAIn.ConferenceRoom.repository;
-
-import com.ssafy.brAIn.ConferenceRoom.entity.ConferenceRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConferenceRoomRepository extends JpaRepository<ConferenceRoom, Integer> {
-}
