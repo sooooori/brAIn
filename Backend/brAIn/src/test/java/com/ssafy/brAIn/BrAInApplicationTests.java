36522c6328f8a2a45b03cd2945bfa4aa3f010a10
박병준 <h0568@naver.com>
2024-07-22 10:02:52 +0900

    [feature] 스프링부트 생성

---
+++ Backend/brAIn/src/test/java/com/ssafy/brAIn/BrAInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.brAIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrAInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
