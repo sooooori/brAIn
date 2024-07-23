593ecc07705b8d8402f859a3f81dfb33f62fec5d
박병준 <h0568@naver.com>
2024-07-23 14:57:55 +0900

    [fix] DBschema fix

--- Backend/brAIn/src/main/java/com/ssafy/brAIn/like/repository/LikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.brAIn.like.repository;
-
-import com.ssafy.brAIn.like.entity.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}
