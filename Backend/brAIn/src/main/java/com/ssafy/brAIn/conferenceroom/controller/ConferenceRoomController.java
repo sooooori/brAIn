4d031ddff828326d22e73bc0863034a3b7c2127d
박병준 <h0568@naver.com>
2024-07-29 11:11:05 +0900

    Fix: tempConfig

--- Backend/brAIn/src/main/java/com/ssafy/brAIn/conferenceroom/controller/ConferenceRoomController.java
+++ Backend/brAIn/src/main/java/com/ssafy/brAIn/conferenceroom/controller/ConferenceRoomController.java
@@ -1,10 +1,17 @@
 package com.ssafy.brAIn.conferenceroom.controller;
 
+import com.ssafy.brAIn.conferenceroom.entity.ConferenceRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/conference")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
 public class ConferenceRoomController {
-
+//    @GetMapping("/{roomId}")
+//    public ResponseEntity<?> getConferenceRoom(@PathVariable String roomId) {
+//        return ResponseEntity.status(200).body();
+//    }
 }
