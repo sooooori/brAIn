9176bd6f32742ef65e18448626760dc2e281070f
강도원 <3120kang@naver.com>
2024-08-01 14:08:00 +0900

    Feat: AI assistant 생성 테스트 추가

--- Backend/brAIn/src/main/java/com/ssafy/brAIn/ai/service/AIService.java
+++ Backend/brAIn/src/main/java/com/ssafy/brAIn/ai/service/AIService.java
@@ -2,12 +2,33 @@ package com.ssafy.brAIn.ai.service;
 
 import com.ssafy.brAIn.ai.response.AIAssistant;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AIService {
 
-    public AIAssistant makeAIAssistant(AIAssistant aiAssistant) {
+    private final WebClient webClient;
 
+    public AIService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public AIAssistant makeAIAssistant(String subject) {
+        String url="/thread/start";
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("subject", subject);
+
+        return webClient
+                .post()
+                .uri(url)
+                .bodyValue(requestBody)
+                .retrieve().bodyToMono(AIAssistant.class)
+                .block();
 
     }
 }
--- Backend/brAIn/src/main/java/com/ssafy/brAIn/conferenceroom/controller/ConferenceRoomController.java
+++ Backend/brAIn/src/main/java/com/ssafy/brAIn/conferenceroom/controller/ConferenceRoomController.java
@@ -35,10 +35,10 @@ public class ConferenceRoomController {
         ConferenceRoom cr = conferenceRoomRequest.toConferenceRoom();
         ConferenceRoom saveCr = conferenceRoomService.save(cr);
         token = token.split(" ")[1];
-        Claims claims = JwtUtil.extractToken(token);
-        String email = claims.get("email").toString();
-        Member member = memberService.findByEmail(email).orElse(null);
-        memberHistoryService.createRoom(saveCr, member);
+        //Claims claims = JwtUtil.extractToken(token);
+        //String email = claims.get("email").toString();
+        //Member member = memberService.findByEmail(email).orElse(null);
+        //memberHistoryService.createRoom(saveCr, member);
         ConferenceRoomResponse crr = new ConferenceRoomResponse(cr);
         return ResponseEntity.status(201).body(crr);
     }
